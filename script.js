dcdfed256303994e72768725621e88138fda31b6
adriandelgg <adriandelgadoc1@gmail.com>
2021-02-24 20:28:35 -0800

    Added HTML & CSS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const url = 'https://coingecko.com/api/documentations/v3';
-const allCoinsUrl = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cvenus%2Cswipe%2Cusd-coin%2Ctether%2Cbinance-usd%2Cethereum%2Clitecoin%2Cripple%2Cbitcoin-cash%2Cpolkadot%2Cchainlink%2Cdai%2Cfilecoin%2Cbinancecoin%2Cbinance-eth&vs_currencies=usd';
+const allCoinsUrl = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cvenus%2Cswipe%2Cusd-coin%2Ctether%2Cbinance-usd%2Cethereum%2Clitecoin%2Cripple%2Cbitcoin-cash%2Cpolkadot%2Cchainlink%2Cdai%2Cfilecoin%2Cbinancecoin%2Cbinance-eth%2Ccardano%2Cvai&vs_currencies=usd';
 
 async function getData() {
     try {
@@ -16,6 +16,7 @@ async function getData() {
 
 async function getCoinPrice(coin) {
     const jsonObject = await getData();
+    console.log(jsonObject);
     return jsonObject[coin].usd;
 }
 
